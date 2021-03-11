28246c2005c5ed0d00f83444b94b079bc19730c9
imjeongwoo <limjeong1219@gmail.com>
2021-03-11 20:20:27 +0900

    Close #1 POST테스트 완료

--- SandBox/PhoneLogin/PhoneLogin/ViewController.swift
+++ SandBox/PhoneLogin/PhoneLogin/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
@@ -13,7 +14,35 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func btnTapped(_ sender: Any) {
+        postTest()
+    }
+    
+    func postTest() {
+        let url = "https://ptsv2.com/t/ifzb7-1615460020/post"
+        var request = URLRequest(url: URL(string: url)!)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.timeoutInterval = 10
+        
+        // POST 로 보낼 정보
+        let params = ["id":"아이디", "pw":"패스워드"] as Dictionary
+        
+        // httpBody 에 parameters 추가
+        do {
+            try request.httpBody = JSONSerialization.data(withJSONObject: params, options: [])
+        } catch {
+            print("http Body Error")
+        }
+        
+        AF.request(request).responseString { (response) in
+            switch response.result {
+            case .success:
+                print("POST 성공")
+            case .failure(let error):
+                print("🚫 Alamofire Request Error\nCode:\(error._code), Message: \(error.errorDescription!)")
+            }
+        }
+    }
 }
 
