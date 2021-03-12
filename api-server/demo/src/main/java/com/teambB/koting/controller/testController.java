77e27ae3b39a743a93ae23997b6055abc0b02dae
hanjongho <lgwl81@gmail.com>
2021-03-12 21:09:42 +0900

    request test

--- api-server/demo/src/main/java/com/teambB/koting/controller/testController.java
+++ api-server/demo/src/main/java/com/teambB/koting/controller/testController.java
@@ -17,7 +17,8 @@ public class testController {
   private HashMap<String, String> dic = new HashMap<String, String>();
 
   @PostMapping("/auth")
-  public void sendCode(@RequestBody JSONObject object) {
+  public boolean sendCode(@RequestBody JSONObject object) {
+    /*
     String phoneNumber = object.get("phoneNumber").toString();
     String api_key = "NCSRF0PYIQASDVPU";
     String api_secret = "CR2RF1F8AWBNK406P1RD51VGBWK1881M";
@@ -39,14 +40,14 @@ public class testController {
     } catch (CoolsmsException e) {
       System.out.println(e.getMessage());
       System.out.println(e.getCode());
-    }
+    }*/
+    return true;
   }
 
   @PostMapping("/auth/number")
   public boolean checkCode(@RequestBody JSONObject object) {
     String phoneNumber = object.get("phoneNumber").toString();
     String code = object.get("code").toString();
-
     if (dic.get(phoneNumber).equals(code)) {  // 인증번호 일치
       // 2가지 경우
       // 1. 가입 된 경우   -> 필요한 정보들 전체 리턴
