89baa1e6f6bfa8b461fefbb5f3d55048fe1cb7e5
hanjongho <lgwl81@gmail.com>
2021-03-12 17:26:32 +0900

    test url 삭제

--- api-server/demo/src/main/java/com/teambB/koting/controller/testController.java
+++ api-server/demo/src/main/java/com/teambB/koting/controller/testController.java
@@ -4,14 +4,13 @@ import com.teambB.koting.domain.MemberTest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 public class testController {
 
-  @PostMapping("/test")
+  @PostMapping("/")
   public Object testMapping(@RequestBody Object params) {
     return params;
   }
