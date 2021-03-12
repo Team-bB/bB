77112981f98596a9caa9fa65ef13e33e22aa8627
hanjongho <lgwl81@gmail.com>
2021-03-12 16:46:02 +0900

    Json 받기 테스트

--- api-server/demo/src/main/java/com/teambB/koting/controller/testController.java
+++ api-server/demo/src/main/java/com/teambB/koting/controller/testController.java
@@ -3,6 +3,7 @@ package com.teambB.koting.controller;
 import com.teambB.koting.domain.MemberTest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,9 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class testController {
 
   @PostMapping("/test")
-  public String testMapping(@RequestParam("id") String id, @RequestParam("pw") String pw) {
-    String tmp = "id: " + id + " pw: "+ pw + " 잘 받았습니다 ^___^";
-    return tmp;
+  public Object testMapping(@RequestBody Object params) {
+    return params;
   }
 
   @GetMapping("/")
