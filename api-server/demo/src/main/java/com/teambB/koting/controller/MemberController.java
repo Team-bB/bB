eb7e7677255e0edbe9c7f79e0d68b7559d39d3aa
hanjongho <lgwl81@gmail.com>
2021-03-15 19:02:02 +0900

    이메일 인증 함수 작성

--- api-server/demo/src/main/java/com/teambB/koting/controller/MemberController.java
+++ api-server/demo/src/main/java/com/teambB/koting/controller/MemberController.java
@@ -1,20 +1,68 @@
 package com.teambB.koting.controller;
 
+import com.teambB.koting.domain.Meeting;
 import com.teambB.koting.domain.Member;
+import com.teambB.koting.repository.MemberRepository;
+import com.teambB.koting.service.MemberService;
+import java.io.UnsupportedEncodingException;
 import java.util.Random;
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import javax.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
 import org.json.simple.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.HashMap;
 import net.nurigo.java_sdk.api.Message;
 import net.nurigo.java_sdk.exceptions.CoolsmsException;
 
 @RestController
+@RequiredArgsConstructor
 public class MemberController {
 
+  @Autowired private final MemberService memberService;
+  @Autowired private final MemberRepository memberRepository;
+  @Autowired private JavaMailSender javaMailSender;
+  @Autowired EntityManager em;
   private HashMap<String, String> dic = new HashMap<String, String>();
+
+  @GetMapping("/")
+  public Object testGet(@RequestBody Object params) {
+    return params;
+  }
+
+  @PostMapping("/")
+  public Object testPost(@RequestBody Object params) {
+    return params;
+  }
+
+  @PostMapping("/signUp")
+  public boolean signUp(@RequestBody JSONObject object) throws UnsupportedEncodingException, MessagingException {
+    String email = object.get("email").toString();
+    String authKey = makeRandomString(6);
+    Member member = Member.createMember(object, authKey);
+    memberService.join(member);
+    sendMail(email, authKey);
+    return true;
+  }
+
+  @GetMapping("/signUpEmail")
+  public boolean authEmail(@RequestParam("email") String email, @RequestParam("authKey") String authKey) {
+    Member findMember = memberService.findOneByEmail(email);
+    if (findMember.getAuthKey().equals(authKey)) {
+      findMember.setAuthStatus(true);
+      memberRepository.save(findMember);
+      return true;
+    }
+    return false;
+  }
 
   @PostMapping("/auth")
   public boolean sendCode(@RequestBody JSONObject object) {
@@ -23,7 +71,7 @@ public class MemberController {
     String api_key = "NCSRF0PYIQASDVPU";
     String api_secret = "CR2RF1F8AWBNK406P1RD51VGBWK1881M";
     Message coolsms = new Message(api_key, api_secret);
-    String code = makeRandom();
+    String code = makeRandomNumber(6);
     String message = "[코팅] 인증번호[" + code + "]를 입력해주세요.";
 
     System.out.println(code);
@@ -36,6 +84,7 @@ public class MemberController {
     params.put("type", "SMS");
     params.put("text", message);
     params.put("app_version", "test app 1.2");
+
     /*
     try {
       JSONObject obj = (JSONObject) coolsms.send(params);
@@ -46,6 +95,7 @@ public class MemberController {
       return false;
     }
      */
+
     return true;
   }
 
@@ -64,26 +114,50 @@ public class MemberController {
     return false;
   }
 
-  @GetMapping("/")
-  public Object testGet(@RequestBody Object params) {
-    return params;
-  }
-
-
-  @PostMapping("/")
-  public Object testPost(@RequestBody Object params) {
-    return params;
-  }
-
-  public String makeRandom() {
+  public String makeRandomNumber(int length) {
     Random rand = new Random();
     String numStr = "";
-    for (int i = 0; i < 4; i++) {
+    for (int i = 0; i < length; i++) {
       String ran = Integer.toString(rand.nextInt(10));
       numStr += ran;
     }
     System.out.println(numStr);
     return numStr;
+  }
+
+  public String makeRandomString(int length) {
+    StringBuffer buffer = new StringBuffer();
+    Random random = new Random();
+
+    String chars[] = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,1,2,3,4,5,6,7,8,9".split(",");
+
+    for (int i = 0; i < length; i++) {
+      buffer.append(chars[random.nextInt(chars.length)]);
+    }
+    return buffer.toString();
+  }
+
+  public void sendMail(String email, String authKey) throws MessagingException, UnsupportedEncodingException {
+
+    String to = "trhjh@naver.com";
+    String from = "admin@koting.com";
+    String subject = "[Koting] 회원가입 인증메일입니다. ";
+    String url = "http://15.165.143.51/signUpEmail?email=" + email + "&authKey=" + authKey;
+
+    StringBuilder body = new StringBuilder();
+    body.append("<html> <body>");
+    body.append("<div> 아래의 링크를 클릭해주세요!</div>");
+    body.append("<br><div>" + url + "</div");
+    body.append("</body> </html>");
+
+    MimeMessage message = javaMailSender.createMimeMessage();
+    MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(message, true, "UTF-8");
+
+    mimeMessageHelper.setFrom(from,"admin@koting.co.kr");
+    mimeMessageHelper.setTo(to);
+    mimeMessageHelper.setSubject(subject);
+    mimeMessageHelper.setText(body.toString(), true);
+    javaMailSender.send(message);
   }
 }
 
