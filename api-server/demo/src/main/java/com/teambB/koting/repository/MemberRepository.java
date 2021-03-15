97cba67c3a94d49eaa81a7d21bf5462ea71c4a61
hanjongho <lgwl81@gmail.com>
2021-03-15 19:03:06 +0900

    Repository 업데이트

--- api-server/demo/src/main/java/com/teambB/koting/repository/MemberRepository.java
+++ api-server/demo/src/main/java/com/teambB/koting/repository/MemberRepository.java
@@ -27,6 +27,12 @@ public class MemberRepository {
     return em.find(Member.class, id);
   }
 
+  public Member findByEmail(String email) {
+    return em.createQuery("select m from Member m where m.email = :email", Member.class)
+        .setParameter("email", email)
+        .getSingleResult();
+  }
+
   public List<Member> findAll() {
     return em.createQuery("select m from Member m", Member.class)
         .getResultList();
