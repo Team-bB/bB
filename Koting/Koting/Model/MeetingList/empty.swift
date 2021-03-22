4599e2994e97b921671bb9bab8155a4f39d02326
imjeongwoo <limjeong1219@gmail.com>
2021-03-22 18:46:57 +0900

    Add podfile

--- Koting/Koting/Model/MeetingList/empty.swift
+++
@@ -1,8 +0,0 @@
-//
-//  empty.swift
-//  Koting
-//
-//  Created by 임정우 on 2021/03/22.
-//
-
-import Foundation
---
+++ Koting/Koting/REST API/Intro/MailAuthCheck.swift
@@ -0,0 +1,20 @@
+//
+//  MailAuthCheck.swift
+//  Koting
+//
+//  Created by 임정우 on 2021/03/22.
+//
+
+import Foundation
+//import Alamofire
+
+class MailAuthCheck {
+    
+    static let shared = MailAuthCheck()
+
+    
+    private init() {
+        
+    }
+
+}
