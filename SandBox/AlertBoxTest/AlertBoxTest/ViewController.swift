9c51dfa50414db71a5a3cc63a4e321362bc19187
imjeongwoo <limjeong1219@gmail.com>
2021-03-26 01:09:55 +0900

    AlertBox Confrim Test

--- SandBox/AlertBoxTest/AlertBoxTest/ViewController.swift
+++ SandBox/AlertBoxTest/AlertBoxTest/ViewController.swift
@@ -11,9 +11,26 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        makeAlertBox(title: "Test", message: "This is test", text: "확인")
+    }
+    func makeAlertBox(title: String, message: String, text: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: text, style: UIAlertAction.Style.cancel) { action in
+            
+            self.makeAlertBox(title: "테스트", message: "성공", text: "확인")
+            
+        }
+        alertController.addAction(okButton)
+        
+        self.present(alertController, animated: true, completion: nil)
+    }
 }
+
+
 
