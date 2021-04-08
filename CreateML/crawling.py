83857d5ebd644621e0ec8dcab579edaa60ecc006
Jeongwoo Lim <limjeong1219@gmail.com>
2021-04-08 21:15:07 +0900

    Update crawling.py

--- CreateML/crawling.py
+++ CreateML/crawling.py
@@ -1,7 +1,6 @@
-import BASE
+from BASE import *
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 query_list = ["공룡상 남자", "여우상 여자", "곰상 여자", "곰상 남자", "사슴상 여자", "토끼상 여자"]
@@ -10,10 +9,10 @@ for query in query_list:
     URL = f"https://www.google.com/search?q={query}+연예인&tbm=isch"
     driver.get(URL)
 
-    BASE.scroll_down(driver)
-
-    path = BASE.set_path(query)
-
-    BASE.download_img(driver, path)
+    scroll_down(driver)
+    
+    path = set_path(query)
+    
+    download_img(driver, path)
 
 driver.quit()
