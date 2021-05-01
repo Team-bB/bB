dedb2ee8622669fa418fc79594ed877ee1610898
hanjongho <lgwl81@gmail.com>
2021-05-01 22:07:16 +0900

    받은 미팅 신청/ 요청한 미팅 리스트 Json 리턴해주는 Controller 작성

--- api-server/demo/src/main/java/com/teambB/koting/controller/ApplyController.java
+++ api-server/demo/src/main/java/com/teambB/koting/controller/ApplyController.java
@@ -6,11 +6,15 @@ import com.teambB.koting.domain.Member;
 import com.teambB.koting.service.ApplyService;
 import com.teambB.koting.service.MeetingService;
 import com.teambB.koting.service.MemberService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,6 +24,46 @@ public class ApplyController {
   @Autowired private final MeetingService meetingService;
   @Autowired private final MemberService memberService;
   @Autowired private final ApplyService applyService;
+
+  @GetMapping("/applies")
+  public JSONObject getMyMeetingInfo(@RequestParam("account_id") String accountId) {
+    JSONObject retObject = new JSONObject();
+    JSONArray jArray = new JSONArray();
+
+    Member member = memberService.findOneByAccountId(accountId);
+    Meeting myMeeting = member.getMyMeeting();
+    if (myMeeting != null) {
+      List<Apply> participants = myMeeting.getParticipants();
+      JSONObject myInfo = new JSONObject();
+      for (int i = 0; i < participants.size(); i++) {
+        myInfo.put("age", participants.get(i).getMember().getAge());
+        myInfo.put("animal_idx", participants.get(i).getMember().getAnimalIdx());
+        myInfo.put("height", participants.get(i).getMember().getHeight());
+        myInfo.put("college", participants.get(i).getMember().getCollege());
+        myInfo.put("major", participants.get(i).getMember().getMajor());
+        myInfo.put("sex", participants.get(i).getMember().getSex());
+        jArray.add(myInfo);
+      }
+      retObject.put("myMeeting", jArray);
+    }
+
+    JSONArray jArray2 = new JSONArray();
+    if (member.getApplies() != null) {
+      List<Apply> applies = member.getApplies();
+      JSONObject myApplies = new JSONObject();
+      for (int i = 0; i < applies.size(); i++) {
+        myApplies.put("age", applies.get(i).getMember().getAge());
+        myApplies.put("animal_idx", applies.get(i).getMember().getAnimalIdx());
+        myApplies.put("height", applies.get(i).getMember().getHeight());
+        myApplies.put("college", applies.get(i).getMember().getCollege());
+        myApplies.put("major", applies.get(i).getMember().getMajor());
+        myApplies.put("sex", applies.get(i).getMember().getSex());
+        jArray2.add(myApplies);
+      }
+      retObject.put("myApplies", jArray2);
+    }
+    return retObject;
+  }
 
   @PostMapping("/applies")
   public JSONObject applyMeeting(@RequestBody JSONObject object) {
