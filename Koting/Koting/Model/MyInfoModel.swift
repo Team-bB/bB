9138e34ff083a5766fc90981d25a8f1e8290b072
Jeongwoo Lim <limjeong1219@gmail.com>
2021-05-07 22:59:19 +0900

    Update MyInfoModel.swift
    오타수정

--- Koting/Koting/Model/MyInfoModel.swift
+++ Koting/Koting/Model/MyInfoModel.swift
@@ -10,9 +10,10 @@ import Foundation
 struct MyInfo {
     
     let sections: [String] = [
-        "계정",
-        "이용안내"
+        "이용안내",
+        "계정"
     ]
+    
     let list: [Int : [String]] = [
         0 : ["공지사항", "앱 정보", "문의하기"],
         1 : ["동물상 재측정", "내 정보 수정", "로그아웃", "회원탈퇴"]
