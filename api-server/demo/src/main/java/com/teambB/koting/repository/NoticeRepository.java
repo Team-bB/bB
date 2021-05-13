ba382c7169bf3671a887fb2b0af2288859f27ab5
hanjongho <lgwl81@gmail.com>
2021-05-13 23:14:06 +0900

    공지사항 전체 조회 함수 구현

--- api-server/demo/src/main/java/com/teambB/koting/repository/NoticeRepository.java
+++ api-server/demo/src/main/java/com/teambB/koting/repository/NoticeRepository.java
@@ -1,7 +1,5 @@
 package com.teambB.koting.repository;
 
-import com.teambB.koting.domain.Meeting;
-import com.teambB.koting.domain.Member;
 import com.teambB.koting.domain.Notice;
 import java.util.List;
 import javax.persistence.EntityManager;
@@ -10,23 +8,14 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 @Repository
-@RequiredArgsConstructor
 @Transactional
+@RequiredArgsConstructor
 public class NoticeRepository {
 
   private final EntityManager em;
-
-  public void save(Notice notice) {
-    if (notice.getId() == null) {
-      em.persist(notice);
-    } else {
-      em.merge(notice);
-    }
-  }
 
   public List<Notice> findAll() {
     return em.createQuery("select n from Notice n", Notice.class)
         .getResultList();
   }
-
 }
