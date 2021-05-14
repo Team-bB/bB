4c58367df8b8e75883edff599a494c10c8edbd90
hanjongho <lgwl81@gmail.com>
2021-05-15 03:40:42 +0900

    공지 등록 PostMapping 구현

--- api-server/demo/src/main/java/com/teambB/koting/controller/MeetController.java
+++ api-server/demo/src/main/java/com/teambB/koting/controller/MeetController.java
@@ -5,67 +5,50 @@ import com.teambB.koting.service.MeetingService;
 import com.teambB.koting.domain.Member;
 import com.teambB.koting.service.MemberService;
 import lombok.RequiredArgsConstructor;
-import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-public class MeetController {
+public class MeetingController {
 
   @Autowired private final MemberService memberService;
   @Autowired private final MeetingService meetingService;
 
   @GetMapping("/meetings")
   public JSONObject getMeetingList(@RequestParam("account_id") String account_id) {
-    List<Meeting> meetingList = meetingService.findAll();
-    Member member = memberService.findOneByAccountId(account_id);
     JSONObject retObject = new JSONObject();
-    JSONArray jArray = new JSONArray();
 
-    for (Meeting meeting : meetingList) {
-      if (member.getId() == meeting.getOwnerId()) {
-        continue ;
-      }
-      JSONObject sObject = new JSONObject();
-      JSONObject owner = new JSONObject();
-      Long ownerId = meeting.getOwnerId();
-      Member owner_ = memberService.findOne(ownerId);
-      memberService.setMemberInfo(owner, owner_);
-      meetingService.setMeetingInfo(sObject, owner, meeting);
-      jArray.add(sObject);
+    Member member = memberService.findOneByAccountId(account_id);
+
+    if (meetingService.findAll() != null) {
+      retObject.put("meeting", meetingService.getMeetingList(member));
     }
-    retObject.put("meeting", jArray); //배열을 넣음
-
-    JSONObject myInfo = new JSONObject();
-    JSONObject owner = new JSONObject();
-    memberService.setMemberInfo(owner, member);
 
     if (member.getMyMeetingId() != null) {
+      JSONObject owner = memberService.setMemberInfo(member);
       Meeting myMeeting = meetingService.findOne(member.getMyMeetingId());
-      meetingService.setMeetingInfo(myInfo, owner, myMeeting);
-      retObject.put("myMeeting", myInfo);
+
+      retObject.put("myMeeting", meetingService.setMeetingInfo(owner, myMeeting));
     }
     return retObject;
   }
 
-
   @Transactional
   @PostMapping("/meetings")
   public JSONObject createMeeting(@RequestBody JSONObject object) {
-
     JSONObject retObject = new JSONObject();
+
     String accountId = object.get("account_id").toString();
     String players = object.get("players").toString();
     String link = object.get("link").toString();
+
     Member member = memberService.findOneByAccountId(accountId);
     if (member.getMyMeetingId() != null) {
       retObject.put("result", "createFail");
@@ -76,15 +59,6 @@ public class MeetController {
       member.setMyMeetingId(meeting.getId());
       retObject.put("result", "createSuccess");
     }
-    return retObject;
-  }
-
-  @DeleteMapping("/meetings")
-  public JSONObject deleteMeeting(@RequestBody JSONObject object) {
-    JSONObject retObject = new JSONObject();
-    String accountId = object.get("account_id").toString();
-    memberService.clearMyMeetingId(accountId);
-    retObject.put("result", "true");
     return retObject;
   }
 }
--- api-server/demo/src/main/java/com/teambB/koting/controller/NoticeController.java
+++ api-server/demo/src/main/java/com/teambB/koting/controller/NoticeController.java
@@ -8,6 +8,8 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -27,10 +29,16 @@ public class NoticeController {
       temp.put("id", all.get(i).getId());
       temp.put("title", all.get(i).getTitle());
       temp.put("content", all.get(i).getContent());
-      temp.put("date", all.get(i).getDate());
+      temp.put("date", all.get(i).getCreateDate());
       array.add(temp);
     }
     retObect.put("notice", array);
     return retObect;
+  }
+
+  @PostMapping("/notice")
+  public void createNotice(@RequestBody JSONObject object) {
+    noticeService.join(
+        Notice.createNotice(object.get("title").toString(), object.get("content").toString()));
   }
 }
