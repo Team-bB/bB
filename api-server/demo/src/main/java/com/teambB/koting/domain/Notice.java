2694d88718a470918b65713931b8a5e15624bfb9
hanjongho <lgwl81@gmail.com>
2021-05-15 03:42:21 +0900

    공지 생성 함수 구현 및 날짜 LocalDateTime 변경

--- api-server/demo/src/main/java/com/teambB/koting/domain/Notice.java
+++ api-server/demo/src/main/java/com/teambB/koting/domain/Notice.java
@@ -1,5 +1,6 @@
 package com.teambB.koting.domain;
 
+import java.time.LocalDateTime;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -23,5 +24,13 @@ public class Notice {
   @Column(columnDefinition = "LONGTEXT")
   private String content;
 
-  private String date;
+  private LocalDateTime createDate;
+
+  public static Notice createNotice(String title, String content) {
+    Notice notice = new Notice();
+    notice.setTitle(title);
+    notice.setContent(content);
+    notice.setCreateDate(LocalDateTime.now());
+    return notice;
+  }
 }
