5d8a4c9c3f0558d7ea409e464438bfb4f8bd0417
imjeongwoo <limjeong1219@gmail.com>
2021-05-20 21:02:28 +0900

    Update Podfile

--- Koting/Koting/Resources/StorageManager.swift
+++ Koting/Koting/Resources/StorageManager.swift
@@ -4,67 +4,67 @@
 //
 //  Created by 임정우 on 2021/05/16.
 //
-
-import Foundation
-import FirebaseStorage
-
-final class StorageManager {
-    
-    static let shared = StorageManager()
-    
-    private init() {}
-    
-    private let storage = Storage.storage().reference()
-    
-    /*
-     /images/token_profile_picture.png
-     */
-    
-    public typealias UploadPictureCompletion = (Result<String,Error>) -> Void
-    /// Firebase에 사진 업로드 후 completion with url string to download
-    public func uploadProfilePicture(with data: Data, fileName: String, completion: @escaping UploadPictureCompletion) {
-        
-        storage.child("image/\(fileName)").putData(data, metadata: nil) { metadata, error in
-            guard error == nil else {
-                
-                print("사진 업로드 실패")
-                completion(.failure(StorageErrors.failedToUpload))
-                return
-            }
-            
-            self.storage.child("image/\(fileName)").downloadURL { url, error in
-                
-                guard let url = url else {
-                    print("failedToGetDownloadURL")
-                    completion(.failure(StorageErrors.failedToGetDownloadURL))
-                    
-                    return
-                }
-                
-                let urlString = url.absoluteString
-                print("download url returned: \(urlString)")
-                completion(.success(urlString))
-            }
-        }
-    }
-    
-    public enum StorageErrors: Error {
-        case failedToUpload
-        case failedToGetDownloadURL
-    }
-    
-    public func downloadURL(for path: String, completion: @escaping (Result<URL, Error>) -> Void) {
-        
-        let reference = storage.child(path)
-        
-        reference.downloadURL { url, error in
-            guard let url = url, error == nil else {
-                completion(.failure(StorageErrors.failedToGetDownloadURL))
-                return
-            }
-            
-            completion(.success(url))
-        }
-    }
-    
-}
+//
+//import Foundation
+//import FirebaseStorage
+//
+//final class StorageManager {
+//    
+//    static let shared = StorageManager()
+//    
+//    private init() {}
+//    
+//    private let storage = Storage.storage().reference()
+//    
+//    /*
+//     /images/token_profile_picture.png
+//     */
+//    
+//    public typealias UploadPictureCompletion = (Result<String,Error>) -> Void
+//    /// Firebase에 사진 업로드 후 completion with url string to download
+//    public func uploadProfilePicture(with data: Data, fileName: String, completion: @escaping UploadPictureCompletion) {
+//        
+//        storage.child("image/\(fileName)").putData(data, metadata: nil) { metadata, error in
+//            guard error == nil else {
+//                
+//                print("사진 업로드 실패")
+//                completion(.failure(StorageErrors.failedToUpload))
+//                return
+//            }
+//            
+//            self.storage.child("image/\(fileName)").downloadURL { url, error in
+//                
+//                guard let url = url else {
+//                    print("failedToGetDownloadURL")
+//                    completion(.failure(StorageErrors.failedToGetDownloadURL))
+//                    
+//                    return
+//                }
+//                
+//                let urlString = url.absoluteString
+//                print("download url returned: \(urlString)")
+//                completion(.success(urlString))
+//            }
+//        }
+//    }
+//    
+//    public enum StorageErrors: Error {
+//        case failedToUpload
+//        case failedToGetDownloadURL
+//    }
+//    
+//    public func downloadURL(for path: String, completion: @escaping (Result<URL, Error>) -> Void) {
+//        
+//        let reference = storage.child(path)
+//        
+//        reference.downloadURL { url, error in
+//            guard let url = url, error == nil else {
+//                completion(.failure(StorageErrors.failedToGetDownloadURL))
+//                return
+//            }
+//            
+//            completion(.success(url))
+//        }
+//    }
+//    
+//}
