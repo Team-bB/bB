0b3d3fa1673cb0b47bf32dcd68c1934b8390110a
imjeongwoo <limjeong1219@gmail.com>
2021-05-24 15:55:07 +0900

    채팅서버 회원탈퇴 구현완료

---
+++ Koting/Koting/Model/WithdrawalDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  WithdrawalDelegate.swift
+//  Koting
+//
+//  Created by 임정우 on 2021/05/24.
+//
+
+import Foundation
+
+protocol WithdrawalDelegate {
+    
+    func WithdrawalButtonTapped()
+}
