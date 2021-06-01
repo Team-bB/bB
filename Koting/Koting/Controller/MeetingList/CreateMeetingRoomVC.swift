cacc978911550c96c059328930d9855976010ff3
honghwahyeong <ghdghkgud@dgu.ac.kr>
2021-06-01 16:54:19 +0900

    미팅 개설시 미팅 인원 선택 피커뷰 -> 버튼으로 변경

--- Koting/Koting/Controller/MeetingList/CreateMeetingRoomVC.swift
+++ Koting/Koting/Controller/MeetingList/CreateMeetingRoomVC.swift
@@ -11,28 +11,33 @@ import PanModal
 
 class CreateMeetingRoomVC: UIViewController {
     //MARK: Outlet설정
-    @IBOutlet weak var participantsNumber: UITextField!
     @IBOutlet weak var shortComment: UITextView!
     @IBOutlet weak var createMeetingRoomBtn: UIButton!
+    @IBOutlet var participantsButton: [UIButton]!
     
-    let participantsPicker = UIPickerView()
-    let participantsArray: [String] = {
-        let array: [String] = ["1 : 1", "2 : 2", "3 : 3", "4 : 4"]
-        return array
-    }()
+//    let participantsPicker = UIPickerView()
+//    let participantsArray: [String] = {
+//        let array: [String] = ["1 : 1", "2 : 2", "3 : 3", "4 : 4"]
+//        return array
+//    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        participantsNumber.tintColor = UIColor.clear
-        participantsNumber.delegate = self
+//        participantsNumber.tintColor = UIColor.clear
+//        participantsNumber.delegate = self
         shortComment.delegate = self
         //participantsNumber.becomeFirstResponder()
         shortComment.layer.borderWidth = 0.5
         shortComment.layer.borderColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
         shortComment.layer.cornerRadius = 10
+        for button in participantsButton {
+            button.layer.borderWidth = 0.5
+            button.layer.borderColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+            button.layer.cornerRadius = 10
+        }
         
-        createPicker()
+        //createPicker()
     }
     override func viewWillAppear(_ animated: Bool) {
         self.addKeyboardNotifications()
@@ -50,8 +55,21 @@ class CreateMeetingRoomVC: UIViewController {
         createMeeting()
     }
     
+    @IBAction func participantsButtonSelected(_ sender: UIButton) {
+        participantsButton.forEach({ $0.tintColor = #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)})
+        sender.tintColor = #colorLiteral(red: 0.9960784314, green: 0.4941176471, blue: 0.2117647059, alpha: 1)
+    }
+    
+    func getParticipants() -> Participants {
+        for (index, button) in participantsButton.enumerated() {
+            if button.tintColor == #colorLiteral(red: 0.9960784314, green: 0.4941176471, blue: 0.2117647059, alpha: 1) {
+                return Participants(rawValue: index) ?? .oneToOne
+            }
+        }
+        return .oneToOne
+    }
     func createMeeting() {
-        CreateMeetingRoomAPI.shared.post(numberOfParticipants: participantsNumber, shortComment: shortComment) { [weak self] result in
+        CreateMeetingRoomAPI.shared.post(numberOfParticipants: getParticipants().getNumberOfParticipants(), shortComment: shortComment) { [weak self] result in
             
             guard let strongSelf = self else { return }
             
@@ -80,51 +98,51 @@ class CreateMeetingRoomVC: UIViewController {
 }
 
 // MARK:- Picker 관련
-extension CreateMeetingRoomVC: UIPickerViewDelegate, UIPickerViewDataSource {
+extension CreateMeetingRoomVC {
     
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return participantsArray.count
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return participantsArray[row]
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        participantsNumber.text = participantsArray[row]
-    }
-    
-    func createPicker(){
-        participantsPicker.delegate = self
-        participantsPicker.dataSource = self
-        participantsNumber.inputView = participantsPicker
-        
-        createToolBar()
-    }
-    
-    func createToolBar() {
-        let toolBar = UIToolbar()
-        let doneBtn = UIBarButtonItem(title: "확인", style: .done, target: self, action: #selector(buttonAction))
-        doneBtn.tintColor = .black
-        let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
-        toolBar.sizeToFit()
-        toolBar.setItems([flexibleSpace,doneBtn], animated: true)
-        toolBar.isUserInteractionEnabled = true
-        
-        participantsNumber.inputAccessoryView = toolBar
-    }
+//    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+//        return 1
+//    }
+//
+//    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+//        return participantsArray.count
+//    }
+//
+//    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+//        return participantsArray[row]
+//    }
+//
+//    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+//        participantsNumber.text = participantsArray[row]
+//    }
+//
+//    func createPicker(){
+//        participantsPicker.delegate = self
+//        participantsPicker.dataSource = self
+//        participantsNumber.inputView = participantsPicker
+//
+//        createToolBar()
+//    }
+//
+//    func createToolBar() {
+//        let toolBar = UIToolbar()
+//        let doneBtn = UIBarButtonItem(title: "확인", style: .done, target: self, action: #selector(buttonAction))
+//        doneBtn.tintColor = .black
+//        let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
+//        toolBar.sizeToFit()
+//        toolBar.setItems([flexibleSpace,doneBtn], animated: true)
+//        toolBar.isUserInteractionEnabled = true
+//
+//        participantsNumber.inputAccessoryView = toolBar
+//    }
     //화면 클릭시 입력창 내리기
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
         self.view.endEditing(true)
     }
-    
-    @objc func buttonAction() {
-        participantsNumber.resignFirstResponder()
-    }
+//
+//    @objc func buttonAction() {
+//        participantsNumber.resignFirstResponder()
+//    }
     
     //MARK:- KEY BOARD 관련
     func addKeyboardNotifications(){
@@ -150,12 +168,12 @@ extension CreateMeetingRoomVC: UITextFieldDelegate, UITextViewDelegate {
 //        textField.resignFirstResponder()
 //        return true
 //    }
-    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
-        if textField == participantsNumber {
-            participantsNumber.text = participantsArray[participantsPicker.selectedRow(inComponent: 0)]
-        }
-        return true
-    }
+//    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+//        if textField == participantsNumber {
+//            participantsNumber.text = participantsArray[participantsPicker.selectedRow(inComponent: 0)]
+//        }
+//        return true
+//    }
 }
 
 // MARK:- PanModal 설정
@@ -189,7 +207,7 @@ extension CreateMeetingRoomVC: PanModalPresentable {
 // MARK:- 추가구현 함수
 extension CreateMeetingRoomVC {
     private func checkTextFiled() -> Bool {
-        if participantsNumber.text == "" || shortComment.text == "" {
+        if shortComment.text == "" {
             return false
         }
         
