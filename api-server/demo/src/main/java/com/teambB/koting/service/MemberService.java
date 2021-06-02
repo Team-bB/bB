e71ce4d8c51c4dfe12332653b7be496a36d793a0
hanjongho <lgwl81@gmail.com>
2021-06-02 20:48:48 +0900

    중복 코드 삭제

--- api-server/demo/src/main/java/com/teambB/koting/service/MemberService.java
+++ api-server/demo/src/main/java/com/teambB/koting/service/MemberService.java
@@ -108,7 +108,6 @@ public class MemberService {
     retObject.put("college", member.getCollege());
     retObject.put("nickname", member.getNickname());
     retObject.put("major", member.getMajor());
-    retObject.put("nickname", member.getNickname());
     retObject.put("sex", member.getSex());
     retObject.put("mbti", member.getMbti());
 
