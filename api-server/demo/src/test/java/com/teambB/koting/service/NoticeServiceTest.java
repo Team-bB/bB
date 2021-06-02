93e02bebea9e8e8e8342636b0d47990232eaad00
hanjongho <lgwl81@gmail.com>
2021-06-02 18:16:08 +0900

    알람 테스트 코드 작성

--- api-server/demo/src/test/java/com/teambB/koting/service/NoticeServiceTest.java
+++ api-server/demo/src/test/java/com/teambB/koting/service/NoticeServiceTest.java
@@ -3,6 +3,7 @@ package com.teambB.koting.service;
 import static org.junit.Assert.*;
 
 import com.teambB.koting.domain.Notice;
+import com.teambB.koting.firebase.FirebaseCloudMessageService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,4 +29,18 @@ public class NoticeServiceTest {
     //then
     assertEquals(notice, noticeService.findOne(id));
   }
+
+  @Test
+  public void 알람테스트() throws Exception {
+      //given
+    FirebaseCloudMessageService firebaseCloudMessageService = new FirebaseCloudMessageService();
+    firebaseCloudMessageService.sendMessageTo("e7b-CPJ6D0Odso8I27WgWh:APA91bGQg3ilrMTqvDoypdNvX-yq_QDpaB5p9j-89gGtIw9kqJx9PtEoknAAV7le0rO3C3O2flrC0TxseaWqHNVcvs26MeZwXrl-MUfNinJaIKqIXOlyQT8xQINiTX6_dhVmsVHiohHs"
+        , "정우형", "이제 초밥시킬까염");
+      //when
+
+
+      //then
+
+  }
+
 }
