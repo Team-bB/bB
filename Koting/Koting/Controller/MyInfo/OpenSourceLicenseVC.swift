1a487e0e363a1fc1ff501dfb41c003a657765451
imjeongwoo <limjeong1219@gmail.com>
2021-06-03 15:57:09 +0900

    resolved #55

--- Koting/Koting/Controller/MyInfo/OpenSourceLicenseVC.swift
+++ Koting/Koting/Controller/MyInfo/OpenSourceLicenseVC.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class OpenSourceLicenseVC: UIViewController {
-
+    
+    let license = OpenSourceLicenseModel()
+    
     @IBOutlet weak var textView: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        textView.text = license.text
+        guard let text = textView.text else { return }
+        let attributeString = NSMutableAttributedString(string: text)
+        attributeString.addAttribute(.font, value: UIFont.systemFont(ofSize: 14, weight: .regular), range: NSMakeRange(0, attributeString.length))
+        attributeString.addAttribute(.foregroundColor, value: UIColor.gray, range: NSMakeRange(0, attributeString.length))
+        for name in license.list {
+            let boldStr = name
+            let font = UIFont.systemFont(ofSize: 15, weight: .semibold)
+            attributeString.addAttribute(.font, value: font, range: (text as NSString).range(of: boldStr))
+        }
+        
+        textView.attributedText = attributeString
 
-        // Do any additional setup after loading the view.
     }
 
     @IBAction func buttonTapped(_ sender: Any) {
